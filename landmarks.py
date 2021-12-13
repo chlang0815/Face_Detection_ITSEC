01130d36b6c2d5ab6935723d1f6a9edfab059e0c
chlang0815 <35453350+chlang0815@users.noreply.github.com>
2021-12-13 17:26:15 +0100

    68landmarks

---
+++ landmarks.py
@@ -0,0 +1,79 @@
+import cv2
+import dlib
+import numpy as np
+
+class FaceClassifier():
+    def __init__(self):
+        """Can create the 68Landmarks classifier
+
+        Parameters
+        ----------
+        scaleFactor : double
+            Describes the scaling factor for the method detctMultiscaleGray of OpenCV
+        minNeighbors : int
+            Describes the numbers of minimum Neighbours for the method detectMultiscaleGray of OpenCV
+        """
+
+        self.shape = "shape_predictor_68_face_landmarks.dat"
+
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(self.shape)
+
+    def detector(self):
+        """Returns the classifer
+
+        Returns
+        -------
+        Dlib frontal face detector
+            Returns detector
+        """
+        return self.detector
+
+    def predictor(self):
+        """Returns the classifer
+
+        Returns
+        -------
+        Dlib shape predictor
+            Returns predictor
+        """
+        return self.predictor
+
+    def shape_to_np(self, dlib_shape, dtype="int"):
+        """Converts dlib shape object to numpy array"""
+
+        # Initialize the list of (x,y) coordinates
+        coordinates = np.zeros((dlib_shape.num_parts, 2), dtype=dtype)
+
+        # Loop over all facial landmarks and convert them to a tuple with (x,y) coordinates:
+        for i in range(0, dlib_shape.num_parts):
+            coordinates[i] = (dlib_shape.part(i).x, dlib_shape.part(i).y)
+
+        # Return the list of (x,y) coordinates:
+        return coordinates
+    
+    def calc_lm_ls_for_img(self, img):
+        """Creates the bounding box list for an img. 
+
+        Parameters
+        ----------
+        img : np.ndarray
+            The image on which the classifier should be applied.
+
+        Returns
+        -------
+        [[int, int, int, int],...]
+            Returns a list of bounding boxes which were recognized by the classifier. However, an empty list can also be returned if nothing was detected.
+        """
+        grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        rects = self.detector(grey_img, 0)
+        
+        shape = []
+        for (i, rect) in enumerate(rects):
+              shape.append(self.shape_to_np(self.predictor(grey_img, rect)))
+
+        return shape
+        
+if __name__ == "__main__":
+    x= cv2.CascadeClassifier(cv2.data.haarcascades + '/haarcascade_frontalface_default.xml')
+    print(type(x))
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import file_reader
 import haar_cascade
+import landmarks
+import time
 
 class HaarCascade():
-    def __init__(self,data_set_path="./data/Celeb-real", scaleFactor = 1.3, minNeighbors = 5, number_of_videos = 2) -> None:
+    def __init__(self,data_set_path="./data/Celeb-real", scaleFactor = 1.3, minNeighbors = 5, number_of_videos = 1) -> None:
         self.data_set_path = data_set_path
         self.scaleFactor = scaleFactor
         self.minNeighbors = minNeighbors
@@ -11,6 +13,7 @@ class HaarCascade():
         self.haar_cascade_classifier = haar_cascade.FaceClassifier(scaleFactor,minNeighbors)
         self.vl_dic = self.create_video_dic()
         self.haar_cascade_classifier_result_dic= self.perform_haar_cascade_on_videos()
+
     def create_video_dic(self):
         return self.fr.read_all_frames_for_all_videos()
 
@@ -21,6 +24,7 @@ class HaarCascade():
             frame_dic[frame_i] = self.perfrom_haar_cascade_for_img(frame)
             frame_i += 1
         return frame_dic
+
     def perfrom_haar_cascade_for_img(self,img):
         return self.haar_cascade_classifier.calc_bb_ls_for_img(img)
 
@@ -31,12 +35,49 @@ class HaarCascade():
             video_results[video] = self.perform_haar_cascade_for_gen(self.vl_dic[video])
         return video_results
 
-    
+
+class SixtyEightLandmarks():
+    def __init__(self, data_set_path="./data/Celeb-real", number_of_videos = 1) -> None:
+        self.data_set_path = data_set_path
+        self.number_of_videos = number_of_videos
+        self.fr = file_reader.FileReader(data_set_path)
+        self.landmarks_classifier = landmarks.FaceClassifier()
+        self.vl_dic = self.create_video_dic()
+        self.landmarks_classifier_result_dic= self.perform_landmark_on_videos()
+        
+
+    def create_video_dic(self):
+        return self.fr.read_all_frames_for_all_videos()
+
+    def perform_landmark_for_gen(self,frame_gen):
+        frame_dic = {}
+        frame_i = 0
+        for frame in frame_gen:
+            frame_dic[frame_i] = self.perfrom_landmark_for_img(frame)
+            frame_i += 1
+        return frame_dic
+
+    def perfrom_landmark_for_img(self,img):
+        return self.landmarks_classifier.calc_lm_ls_for_img(img)
+
+    def perform_landmark_on_videos(self):
+        video_results={}
+        dic_keys = list(self.vl_dic.keys())[:self.number_of_videos]
+        for video in dic_keys:
+            video_results[video] = self.perform_landmark_for_gen(self.vl_dic[video])
+        with open('68landmarks.txt', 'w') as f:
+            print(video_results, file=f)
+        return video_results
+
 if __name__ == '__main__':
     rel_path_video = "./data/Celeb-real/id0_0002.mp4"
     data_set_celb_real_dir = "./data/Celeb-real"
-    hcr = HaarCascade(data_set_celb_real_dir)
-    print(hcr.haar_cascade_classifier_result_dic)
+    start = time.time()
+    hcr = SixtyEightLandmarks(data_set_celb_real_dir)
+    end = time.time()
+    
+    print("Time HaarCascade: " , end-start)
+
     
     
     #video_0 = list(hcr.vl_dic.keys())[0]
