fc2b1ea03ec2261d04ce88b7d41fb6ce9a945d2f
Till Sehlen <till.sehlen@gmail.com>
2021-12-20 15:15:38 +0100

    drawer changed

--- drawer.py
+++ drawer.py
@@ -4,19 +4,16 @@ from pathlib import Path
 class Drawer():
     """Class for drawing the specified result dictonary. 
     """
-    def __init__(self,fr ,result_dic, color) -> None:
+    def __init__(self,result_dic, color) -> None:
         """Initilizes the file reader, the result dictonary, and the color.
 
         Parameters
         ----------
-        fr : obj
-            The file reader object.
         result_dic :  {str : dic
             A dictonary that contains the videos as keys and a generator as values. A generator contains a dictonary. This diconary has the videos as keys and the frames as values. Each frame contains a list of results.  
         color : (int, int, int)
             The color value. Each integer should be in the range of 0 to 255.
         """
-        self.fr = fr
         self.result_dic = result_dic
         self.color = color
 
@@ -40,29 +37,6 @@ class Drawer():
         video = self.result_dic[str(Path(rel_path_to_video))]
         return video[frame]
     
-    def read_specific_frame_for_video(self, rel_path_to_video, frame):
-        """Returns an img for the specified video and frame.
-
-        Parameters
-        ----------
-        rel_path_to_video : str
-            Relative path to the video.
-        frame : int
-            The frame number.
-
-        Returns
-        -------
-        np.ndarray
-            Coloured image, with three dimensions.
-        """
-        f_i = 0
-        frame_gen = self.fr.read_all_frames_for_one_video(rel_path_to_video)
-        if(f_i == 0):
-            return next(frame_gen)
-        while(f_i != frame):
-            frame = next(frame_gen)
-            f_i += 1
-            return frame
 
     def show_img(self, img, txt = "DRAWER"):
         """Shows the img 
@@ -80,25 +54,22 @@ class Drawer():
 
 class HaarCascadeDrawer(Drawer):
     
-    def __init__(self,fr,result_dic, color = (0,0,255)) -> None:
+    def __init__(self,result_dic, color = (0,0,255)) -> None:
         """Class for drawing the specified result dictonary. Moreover, special functionality for the Haar cascade drawings.
 
         Parameters
         ----------
-        fr : obj
-            The instance of the file reader class
         result_dic : dic
             Result dictonary that should contain videos as key and dictonarys as values. These dictonarys should contain the frame number as the key and the list containing a/multiple list/s of four numbers as the values.
             For example { video: {frame : [[int, int, int, int], ...]}}
         color : tuple, optional
             The color value. Each integer should be in the range of 0 to 255, by default (0,0,255)
         """
-        super().__init__(fr,result_dic, color)
-        print(result_dic)
+        super().__init__(result_dic, color)
         self.fontScale =0.75
         self.font =cv2.FONT_HERSHEY_SIMPLEX
     
-    def draw_bounding_box_to_img(self, rel_path_to_video, frame):
+    def draw_bounding_box_to_img(self, rel_path_to_video, frame, img):
         """Draws a bouning to the image and returns it.
 
         Parameters
@@ -114,8 +85,7 @@ class HaarCascadeDrawer(Drawer):
             The image
         """
         result_ls = self.return_result_for_video_and_frame(rel_path_to_video, frame)
-        img = self.read_specific_frame_for_video(rel_path_to_video, frame)
-        bb_img = img
+        bb_img = img.copy()
         print("IMG_shape:",img.shape)
         for x, y, w, h in result_ls:
             bb_img = cv2.rectangle(bb_img, (x, y),( x + w, y + h), self.color)
@@ -124,25 +94,23 @@ class HaarCascadeDrawer(Drawer):
 
 class LandmarkDrawer(Drawer):
     
-    def __init__(self,fr,result_dic, color = (0,0,255)) -> None:
+    def __init__(self,result_dic, color = (0,0,255)) -> None:
         """Class for drawing the specified result dictonary. Moreover, special functionality for the Haar cascade drawings.
 
         Parameters
         ----------
-        fr : obj
-            The instance of the file reader class
         result_dic : dic
             Result dictonary that should contain videos as key and dictonarys as values. These dictonarys should contain the frame number as the key and the list containing a/multiple list/s of four numbers as the values.
             For example { video: {frame : [[int, int, int, int], ...]}}
         color : tuple, optional
             The color value. Each integer should be in the range of 0 to 255, by default (0,0,255)
         """
-        super().__init__(fr,result_dic, color)
+        super().__init__(result_dic, color)
 
         self.fontScale =0.75
         self.font =cv2.FONT_HERSHEY_SIMPLEX
     
-    def draw_bounding_box_to_img(self, rel_path_to_video, frame):
+    def draw_bounding_box_to_img(self, rel_path_to_video, frame, img):
         """Draws a bouning to the image and returns it.
 
         Parameters
@@ -158,9 +126,8 @@ class LandmarkDrawer(Drawer):
             The image
         """
         result_ls = self.return_result_for_video_and_frame(rel_path_to_video, frame)
-        img = self.read_specific_frame_for_video(rel_path_to_video, frame)
-        bb_img = img
-        print(result_ls)
+       
+        bb_img = img.copy() 
         print("IMG_shape:",img.shape)
         for face in result_ls:
             for point in face:
--- main.py
+++ main.py
@@ -5,6 +5,31 @@ import time
 import drawer
 import result_writer
 
+def read_specific_frame_for_video(file_reader, rel_path_to_video, frame):
+    """Returns an img for the specified video and frame.
+
+    Parameters
+    ----------
+    rel_path_to_video : str
+        Relative path to the video.
+    frame : int
+        The frame number.
+
+    Returns
+    -------
+    np.ndarray
+        Coloured image, with three dimensions.
+    """
+    f_i = 0
+    frame_gen = file_reader.read_all_frames_for_one_video(rel_path_to_video)
+    if(f_i == 0):
+        return next(frame_gen)
+    while(f_i != frame):
+        frame = next(frame_gen)
+        f_i += 1
+        return frame
+
+
 class HaarCascade():
     def __init__(self,data_set_path="./data/Celeb-real",rel_path_to_write = "./results/Celeb-real", scaleFactor = 1.3, minNeighbors = 5, number_of_videos = 2) -> None:
 
@@ -84,15 +109,21 @@ class SixtyEightLandmarks():
 
 
 if __name__ == '__main__':
-    rel_path_video = "data/Celeb-real/id0_0000.mp4"
+    rel_path_video = 'data/Celeb-real/id23_0006.mp4'
     data_set_celb_real_dir = "./data/Celeb-real"
     rel_path_to_write ="./results/Celeb-real"
     lr = SixtyEightLandmarks()
-    draw = drawer.LandmarkDrawer(lr.fr,lr.landmarks_classifier_result_dic, color=(0,255,100))
-    test_img = draw.draw_bounding_box_to_img(rel_path_video,0)
+    hc = HaarCascade()
+    draw_hc = drawer.HaarCascadeDrawer(hc.haar_cascade_classifier_result_dic, color=(0,0,250))
+    draw = drawer.LandmarkDrawer(lr.landmarks_classifier_result_dic, color=(0,255,100))
+    #print(lr.landmarks_classifier_result_dic.keys())
+    img = read_specific_frame_for_video(hc.fr,rel_path_video,frame=0)
+    img = draw.draw_bounding_box_to_img(rel_path_video,0, img)
+    img = draw_hc.draw_bounding_box_to_img(rel_path_video,0, img)
     #dic = lr.landmarks_classifier_result_dic
     #lr.write_the_results()
-    draw.show_img(test_img)
+   
+    draw_hc.show_img(img)
     """hcr = HaarCascade(data_set_celb_real_dir,rel_path_to_write)
     draw = drawer.HaarCascadeDrawer(hcr.fr,hcr.haar_cascade_classifier_result_dic, color=(255,0,255))
     test_img = draw.draw_bounding_box_to_img(rel_path_video,0)
